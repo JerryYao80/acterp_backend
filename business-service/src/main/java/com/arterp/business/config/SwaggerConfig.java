6a5b611b81c8609cf26936161c82dc533e5f06d8
JerryYao80 <98963023+JerryYao80@users.noreply.github.com>
2025-01-12 10:51:06 +0800

    添加swagger配置

---
+++ business-service/src/main/java/com/arterp/business/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.arterp.business.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.arterp.business.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("ART ERP Business Service API")
+                .description("Business service REST API documentation")
+                .version("1.0.0")
+                .contact(new Contact("ART ERP Team", "https://arterp.com", "contact@arterp.com"))
+                .build();
+    }
+} 
